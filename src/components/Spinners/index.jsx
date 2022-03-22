43c7f44501ddd7835dcd000486f55e5fa5d8fda6
MIGUEL MERCADO SALGADO <mmercado@accom.local>
2022-03-22 19:58:53 +0100

    feat(spinner): first spinner created

---
+++ src/components/Spinners/index.jsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+import FirstSpinner from "./FirstSpinner";
+import "./Spinners.css";
+
+const Spinners = () => {
+  return (
+    <Fragment>
+      <h1>Spinners</h1>
+      <main>
+        <FirstSpinner />
+      </main>
+    </Fragment>
+  );
+};
+
+export default Spinners;
